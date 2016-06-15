e3337ff0d859339d675044baf664de25f2d69467
Aaron Taylor <aaron@hamsterdam.co>
2016-06-14 22:42:47 -0700

    Rename core functions and types

--- tests/NBA/Stats/Tests.hs
+++ tests/NBA/Stats/Tests.hs
@@ -34,7 +34,7 @@ tests = Tasty.testGroup "NBA.Stats" [
             return $ show request == show model,
     propertyStatsExceptionShow Stats.HTTPException "HTTPException",
     propertyStatsExceptionShow Stats.PayloadDecodeError "PayloadDecodeError",
-    propertyStatsExceptionShow Stats.NoMatchingResult "NoMatchingResult",
+    propertyStatsExceptionShow Stats.NoMatchingSplit "NoMatchingSplit",
     propertyStatsExceptionShow Stats.NoMatchingRow "NoMatchingRow",
     propertyStatsExceptionShow Stats.NoKeyInColumns "NoKeyInColumns",
     propertyStatsExceptionShow Stats.NoValueForRowIndex "NoValueForRowIndex",
@@ -42,19 +42,19 @@ tests = Tasty.testGroup "NBA.Stats" [
 
     getStatExpectSuccess
         "Success"
-        (defaultResponseBody [defaultResult])
+        (defaultResponseBody [defaultSplit])
         defaultModel,
 
     getStatsExpectSuccess
         "Success"
-        (defaultResponseBody [defaultResult { Stats.rows = [defaultRow, defaultRow] }])
+        (defaultResponseBody [defaultSplit { Stats.rows = [defaultRow, defaultRow] }])
         [defaultModel, defaultModel],
 
     HUnit.testCase
         "Get stat -> HTTPException StatusCodeException"
         (Catch.catch
             (do
-                _ <- runAction statAction (defaultResponseBody [defaultResult]) HTTPTypes.unauthorized401
+                _ <- runAction statAction (defaultResponseBody [defaultSplit]) HTTPTypes.unauthorized401
                 HUnit.assertFailure "StatusCodeException should have been thrown"
             )
             (\(e :: Stats.StatsException) ->
@@ -62,69 +62,69 @@ tests = Tasty.testGroup "NBA.Stats" [
 
     getStatExpectFailure
         "NoValueForRowIndex"
-        (defaultResponseBody [defaultResult { Stats.rows = [take 2 defaultRow] }])
+        (defaultResponseBody [defaultSplit { Stats.rows = [take 2 defaultRow] }])
         (Stats.NoValueForRowIndex "2")
         "Should not have row value matching for index 2",
 
     getStatExpectFailure
         "NoValueForRowIndex"
-        (defaultResponseBody [defaultResult { Stats.rows = [take 2 defaultRow] }])
+        (defaultResponseBody [defaultSplit { Stats.rows = [take 2 defaultRow] }])
         (Stats.NoValueForRowIndex "2")
         "Should not have row value matching for index 2",
 
     getStatExpectFailure
         "NoMatchingRow (no rows)"
-        (defaultResponseBody [defaultResult { Stats.rows = [] }])
+        (defaultResponseBody [defaultSplit { Stats.rows = [] }])
         (Stats.NoMatchingRow $ show defaultRowIdentifier)
         ("Should not have row with matching identifier: " ++ show defaultRowIdentifier),
 
     getStatExpectFailure
         "NoMatchingRow (no key value)"
-        (defaultResponseBody [defaultResult { Stats.rows = [[]] }])
+        (defaultResponseBody [defaultSplit { Stats.rows = [[]] }])
         (Stats.NoMatchingRow $ show defaultRowIdentifier)
         ("Should not have row with matching identifier: " ++ show defaultRowIdentifier),
 
     getStatExpectFailure
         "NoMatchingRow (JSON parse error for key value)"
-        (let rowWithBadValue = Aeson.Number 99 : defaultRow in defaultResponseBody [defaultResult { Stats.rows = [rowWithBadValue] }])
+        (let rowWithBadValue = Aeson.Number 99 : defaultRow in defaultResponseBody [defaultSplit { Stats.rows = [rowWithBadValue] }])
         (Stats.NoMatchingRow $ show defaultRowIdentifier)
         ("Should not have row with matching identifier: " ++ show defaultRowIdentifier),
 
     getStatExpectFailure
-        "NoMatchingResult"
+        "NoMatchingSplit"
         (defaultResponseBody [])
-        (Stats.NoMatchingResult $ Text.unpack defaultResultName)
-        ("Should not have matching result:" ++ Text.unpack defaultResultName),
+        (Stats.NoMatchingSplit $ Text.unpack defaultSplitName)
+        ("Should not have matching split:" ++ Text.unpack defaultSplitName),
 
     getStatExpectFailure
         "NoKeyInColumns"
-        (defaultResponseBody [defaultResult { Stats.columns = [] }])
+        (defaultResponseBody [defaultSplit { Stats.columns = [] }])
         (Stats.NoKeyInColumns $ Text.unpack defaultColumnsKey)
         ("Should not have key in columns: " ++ Text.unpack defaultColumnsKey),
 
     getStatExpectFailure
         "TableConversionError (type mismatch)"
-        (defaultResponseBody [defaultResult { Stats.rows = [[Aeson.String defaultRowIdentifier, Aeson.String $ Text.pack $ show (a defaultModel), Aeson.String $ b defaultModel, Aeson.Number $ Sci.fromFloatDigits (c defaultModel)]] }])
+        (defaultResponseBody [defaultSplit { Stats.rows = [[Aeson.String defaultRowIdentifier, Aeson.String $ Text.pack $ show (a defaultModel), Aeson.String $ b defaultModel, Aeson.Number $ Sci.fromFloatDigits (c defaultModel)]] }])
         (Stats.TableConversionError "failed to parse field A: expected Integral, encountered String")
         "Should not convert because of field is wrong type",
 
     getStatExpectFailure
         "TableConversionError (missing field)"
-        (defaultResponseBody [defaultResult { Stats.columns = take (length defaultColumns - 1) defaultColumns }])
+        (defaultResponseBody [defaultSplit { Stats.columns = take (length defaultColumns - 1) defaultColumns }])
         (Stats.TableConversionError "key \"C\" not present")
         "Should not convert because field is missing",
 
     getStatExpectFailure
-        "PayloadDecodeError (for Resource)"
-        (Aeson.encode [defaultResult])
-        (Stats.PayloadDecodeError "Error in $: expected Resource, encountered Array")
+        "PayloadDecodeError (for Stats)"
+        (Aeson.encode [defaultSplit])
+        (Stats.PayloadDecodeError "Error in $: expected Stats, encountered Array")
         "Should not be able to decode invalid JSON",
 
     HUnit.testCase
-        "parseJSON invalid :: Parser Result -> Error"
-        (case Aeson.fromJSON $ Aeson.String "foo" :: Aeson.Result Stats.Result of
+        "parseJSON invalid :: Parser Split -> Error"
+        (case Aeson.fromJSON $ Aeson.String "foo" :: Aeson.Result Stats.Split of
             Aeson.Success _ -> HUnit.assertFailure "Parse should not have succeeded"
-            Aeson.Error e -> e @?= "expected Result, encountered String")
+            Aeson.Error e -> e @?= "expected Split, encountered String")
     ]
 
 propertyStatsExceptionShow :: Show a => (String -> a) -> String -> Tasty.TestTree
@@ -147,13 +147,13 @@ expectSuccess action testName responseBody expected =
         (HUnit.assertFailure . Catch.displayException)
 
 getStatsExpectSuccess :: Tasty.TestName -> ByteString.ByteString -> [MockModel] -> Tasty.TestTree
-getStatsExpectSuccess name = expectSuccess (Stats.getStats "mockmodels" defaultResultName defaultParams) $ "Get stats -> " <> name
+getStatsExpectSuccess name = expectSuccess (Stats.getSplitRows "mockmodels" defaultSplitName defaultParams) $ "Get stats -> " <> name
 
 getStatExpectSuccess :: Tasty.TestName -> ByteString.ByteString -> MockModel -> Tasty.TestTree
 getStatExpectSuccess name = expectSuccess statAction $ "Get stat -> " <> name
 
 statAction :: HTTP.Manager -> MonadHTTP.MockHTTP IO MockModel
-statAction = Stats.getStat "mockmodels" defaultResultName defaultColumnsKey defaultRowIdentifier defaultParams
+statAction = Stats.getSplitRow "mockmodels" defaultSplitName defaultColumnsKey defaultRowIdentifier defaultParams
 
 runAction :: (HTTP.Manager -> MonadHTTP.MockHTTP IO a) -> ByteString.ByteString -> HTTPTypes.Status -> IO a
 runAction action responseBody responseStatus = do
@@ -172,23 +172,23 @@ runAction action responseBody responseStatus = do
 defaultParams :: Stats.Parameters
 defaultParams = [("param", Just "value")]
 
-defaultResponseBody :: [Stats.Result] -> ByteString.ByteString
-defaultResponseBody results = Aeson.encode defaultResource { Stats.results = results }
+defaultResponseBody :: [Stats.Split] -> ByteString.ByteString
+defaultResponseBody splits = Aeson.encode defaultStats { Stats.splits = splits }
 
-defaultResource :: Stats.Resource
-defaultResource = Stats.Resource {
-    results = []
+defaultStats :: Stats.Stats
+defaultStats = Stats.Stats {
+    splits = []
 }
 
-defaultResult :: Stats.Result
-defaultResult = Stats.Result {
-    name = defaultResultName,
+defaultSplit :: Stats.Split
+defaultSplit = Stats.Split {
+    name = defaultSplitName,
     columns = defaultColumns,
     rows = [defaultRow]
 }
 
-defaultResultName :: Stats.ResultName
-defaultResultName = "name"
+defaultSplitName :: Stats.SplitName
+defaultSplitName = "name"
 
 defaultColumnsKey :: Stats.Column
 defaultColumnsKey = "key"
